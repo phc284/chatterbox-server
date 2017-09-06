9e687bd4d8c45ed7d9460298470cbc6bd2639849
Hack Reactor Student <student@hackreactor.com>
2017-09-06 12:29:25 -0500

    exported and imported request handler

--- server/request-handler.js
+++ server/request-handler.js
@@ -39,7 +39,7 @@ var requestHandler = function(request, response) {
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
+  headers['Content-Type'] = 'application/json';
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
@@ -52,7 +52,7 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+  response.end('{"hi": "world"}');
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -71,3 +71,5 @@ var defaultCorsHeaders = {
   'access-control-max-age': 10 // Seconds.
 };
 
+module.exports.requestHandler = requestHandler;
+// module.exports.defaultCorsHeaders = defaultCorsHeaders;
